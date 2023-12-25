e3d6d6f67b6800cdd5abdde4744b356d63ad3b68
melvintivane <mtivane@technoplus.co.mz>
2023-12-26 00:37:53 +0200

    feat: Transform Card component into a capital quiz game with Vue 3

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,64 +1,136 @@
-<script setup lang="ts">
-import axios from 'axios'
-import { ref, onMounted } from 'vue'
-
-defineProps<{
-  msg: string
-  question: string
-}>()
-
-const countriesData = ref(null)
-
-const fetchData = async () => {
-  try {
-    const apiUrl = 'https://restcountries.com/v3.1/independent?status=true&fields=languages,capital'
-    const response = await axios.get(apiUrl)
-    countriesData.value = response.data
-  } catch (error) {
-    console.error('Erro ao buscar dados da API:', error)
-  }
-}
-
-onMounted(() => {
-  fetchData()
-  console.log(countriesData)
-})
-</script>
-
 <template>
   <div class="card__container">
     <img src="@/assets/adventure-icon.svg" alt="Adventure Icon" class="img" />
     <h1 class="card__title">{{ msg }}</h1>
     <div class="card__wrapper">
-      <h3 class="card__question">{{ question }}</h3>
-      <button type="button" value="" class="card__button variant">
-        <span class="answer__option">A</span>
-        Button
+      <h3 class="card__question">{{ currentQuestion.question }}</h3>
+      <button
+        v-for="(answer, index) in currentQuestion.answers"
+        :key="index"
+        @click="selectAnswer(answer)"
+        :class="{
+          card__button: true,
+          variant: index !== correctAnswerIndex,
+          answer__correct: index === correctAnswerIndex,
+          answer__wrong: selectedAnswerIndex === index
+        }"
+      >
+        <span class="answer__option">{{ String.fromCharCode(65 + index) }}</span>
+        {{ answer.text }}
       </button>
-      <button type="button" value="" class="card__button answer__correct">
-        <span class="answer__option">A</span>
-        Button
-      </button>
-      <button type="button" value="" class="card__button answer__wrong">
-        <span class="answer__option">A</span>
-        Button
-      </button>
-      <button type="button" value="" class="card__button">
-        <span class="answer__option">A</span>
-        Button
-      </button>
-      <button type="button" value="" class="btn-next">Next</button>
+      <button v-if="showNextButton" @click="nextQuestion" class="btn-next">Next</button>
     </div>
-    <!-- <div class="card__wrapper">
-      <img src="@/assets/congrats-icon.svg" alt="Congrats Icon" class="congrats" />
-
-      <h1 class="h1">Results</h1>
-      <p>You got 0 correct answers.</p>
-
-      <button type="button" value="" class="btn-try__again">Try Again</button>
-    </div> -->
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+const questions = ref([
+  {
+    question: 'What is the capital of Brazil?',
+    answers: [
+      { text: 'Brasília', correct: true },
+      { text: 'Rio de Janeiro', correct: false },
+      { text: 'Buenos Aires', correct: false },
+      { text: 'Lima', correct: false }
+    ]
+  },
+  {
+    question: 'What is the capital of France?',
+    answers: [
+      { text: 'Paris', correct: true },
+      { text: 'Berlin', correct: false },
+      { text: 'London', correct: false },
+      { text: 'Madrid', correct: false }
+    ]
+  },
+  {
+    question: 'What is the capital of Japan?',
+    answers: [
+      { text: 'Tokyo', correct: true },
+      { text: 'Beijing', correct: false },
+      { text: 'Seoul', correct: false },
+      { text: 'Bangkok', correct: false }
+    ]
+  },
+  {
+    question: 'What is the capital of Australia?',
+    answers: [
+      { text: 'Canberra', correct: true },
+      { text: 'Sydney', correct: false },
+      { text: 'Melbourne', correct: false },
+      { text: 'Wellington', correct: false }
+    ]
+  }
+])
+
+const isGameStarted = ref(false)
+const isGameEnded = ref(false)
+const showNextButton = ref(false)
+const currentQuestionIndex = ref(0)
+const currentQuestion = computed(() => questions.value[currentQuestionIndex.value])
+
+const startGame = () => {
+  isGameStarted.value = true
+  isGameEnded.value = false
+  showNextButton.value = false
+  currentQuestionIndex.value = 0
+}
+
+const restartGame = () => {
+  isGameStarted.value = false
+  isGameEnded.value = false
+  showNextButton.value = false
+  currentQuestionIndex.value = 0
+}
+
+const setNextQuestion = () => {
+  resetState()
+  if (currentQuestionIndex.value < questions.value.length) {
+    showQuestion()
+  } else {
+    isGameEnded.value = true
+  }
+}
+
+const showQuestion = () => {
+  showNextButton.value = false
+}
+
+const resetState = () => {
+  clearStatusClass(document.body)
+  showNextButton.value = false
+}
+
+const selectAnswer = (answer: any) => {
+  const correct = answer.correct
+  setStatusClass(document.body, correct)
+  if (questions.value.length > currentQuestionIndex.value + 1) {
+    showNextButton.value = true
+  } else {
+    isGameEnded.value = true
+  }
+}
+
+const setStatusClass = (element: any, correct: any) => {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+const clearStatusClass = (element: any) => {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const nextQuestion = () => {
+  currentQuestionIndex.value++
+  setNextQuestion()
+}
+</script>
 
 <style scoped>
 .card__container {
