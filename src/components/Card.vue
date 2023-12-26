2f48e041ec1cec5468160e3cedcda70a603261c0
melvintivane <mtivane@technoplus.co.mz>
2023-12-26 08:10:34 +0200

    feat: Add game over screen and try again button

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,24 +1,32 @@
 <template>
-  <div class="card__container">
-    <img src="@/assets/adventure-icon.svg" alt="Adventure Icon" class="img" />
-    <h1 class="card__title">{{ msg }}</h1>
-    <div class="card__wrapper">
-      <h3 class="card__question">{{ currentQuestion.question }}</h3>
-      <button
-        v-for="(answer, index) in currentQuestion.answers"
-        :key="index"
-        @click="selectAnswer(answer)"
-        :class="{
-          card__button: true,
-          variant: index !== correctAnswerIndex,
-          answer__correct: index === correctAnswerIndex,
-          answer__wrong: selectedAnswerIndex === index
-        }"
-      >
-        <span class="answer__option">{{ String.fromCharCode(65 + index) }}</span>
-        {{ answer.text }}
-      </button>
-      <button v-if="showNextButton" @click="nextQuestion" class="btn-next">Next</button>
+  <div>
+    <div v-if="!isGameEnded">
+      <div class="card__container">
+        <img src="@/assets/adventure-icon.svg" alt="Adventure Icon" class="img" />
+        <h1 class="card__title">{{ msg }}</h1>
+        <div class="card__wrapper">
+          <h3 class="card__question">{{ currentQuestion.question }}</h3>
+          <button
+            v-for="(answer, index) in currentQuestion.answers"
+            :key="index"
+            @click="selectAnswer(answer)"
+            :class="{
+              card__button: true,
+              variant: index !== correctAnswerIndex,
+              answer__correct: index === correctAnswerIndex,
+              answer__wrong: selectedAnswerIndex === index
+            }"
+          >
+            <span class="answer__option">{{ String.fromCharCode(65 + index) }}</span>
+            {{ answer.text }}
+          </button>
+          <button v-if="showNextButton" @click="nextQuestion" class="btn-next">Next</button>
+        </div>
+      </div>
+    </div>
+    <div v-if="isGameEnded">
+      <h3 class="game-over">Game Over!</h3>
+      <button @click="restartGame" class="btn-try__again">Try Again</button>
     </div>
   </div>
 </template>
@@ -69,6 +77,7 @@ const isGameEnded = ref(false)
 const showNextButton = ref(false)
 const currentQuestionIndex = ref(0)
 const currentQuestion = computed(() => questions.value[currentQuestionIndex.value])
+const isGameOver = ref(false)
 
 const startGame = () => {
   isGameStarted.value = true
@@ -105,6 +114,11 @@ const resetState = () => {
 const selectAnswer = (answer: any) => {
   const correct = answer.correct
   setStatusClass(document.body, correct)
+  if (!correct) {
+    isGameOver.value = true
+    return
+  }
+
   if (questions.value.length > currentQuestionIndex.value + 1) {
     showNextButton.value = true
   } else {
@@ -133,6 +147,10 @@ const nextQuestion = () => {
 </script>
 
 <style scoped>
+.card__container[style*='display: none'] {
+  display: none;
+}
+
 .card__container {
   position: relative;
 }
@@ -140,7 +158,7 @@ const nextQuestion = () => {
 .img {
   position: absolute;
   right: 0;
-  top: -1.3rem;
+  top: -70px;
 }
 
 .card__title {
@@ -151,7 +169,6 @@ const nextQuestion = () => {
 .card__wrapper {
   display: flex;
   flex-direction: column;
-  align-items: center;
   width: 31rem;
   background-color: #fff;
   border-radius: 18px;
@@ -199,7 +216,7 @@ p {
   border-color: transparent;
 }
 
-.answer__correct:hover {
+.answer__correct {
   color: var(--color-white);
   background-color: #60bf88;
   border-color: transparent;
@@ -240,5 +257,12 @@ p {
   font-size: 1.4rem;
   font-weight: 500;
   margin-right: 4rem;
+}
+
+.game-over {
+  font-size: 2rem;
+  color: #ff0000;
+  text-align: center;
+  margin-bottom: 20px;
 }
 </style>
