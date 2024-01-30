b5ea029e305a606b09e6cdf2b1dc0c919fc960a7
melvintivane <melvinshuster47@gmail.com>
2024-01-30 09:26:41 +0200

    feat: configure pinia store to fetch countries

--- src/stores/countries.ts
+++ src/stores/countries.ts
@@ -1,7 +1,27 @@
 import { defineStore } from "pinia";
+import axios from "axios";
 
+interface CountryType {
+	countries: []
+}
 
-
-export const useCountriesStore = defineStore('countries', () => {
-    
+export const useCountriesStore = defineStore('countries', {
+	state: () => {
+		return {
+			countries: [] as CountryType[]
+		}
+	},
+	getters: {
+		getCountries: (state) => state.countries,
+	},
+	actions: {
+		async fetchCountries() {
+			try {
+				const res = await axios.get('https://restcountries.com/v3.1/all?fields=name,capital,flag')
+				this.countries = res.data
+			} catch (error) {
+				return window.alert('Erro ao buscar países.')
+			}
+		}
+	}
 })
