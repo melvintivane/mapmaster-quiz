53f4d9e030e7977bdc7ee6c00e72923a49deb991
melvintivane <melvinshuster47@gmail.com>
2024-02-16 10:45:31 +0200

    feat: add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: {
+          default: '#1d355d'
+        },
+        grey: {
+          default: '#f2f2f2'
+        },
+        border: {
+          default: '#6066d0b3'
+        },
+        text: {
+          default: '#6066d0cc'
+        },
+        question: {
+          default: '#2f527b'
+        },
+        right: {
+          default: '#60bf88'
+        },
+        wrong: {
+          default: '#ea8282'
+        },
+        button: {
+          default: '#f9a826'
+        }
+      }
+    }
   },
   plugins: []
 }
